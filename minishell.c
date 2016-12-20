259b71ab6ce6fec4e9280dd1faa243ea822520d2
Johnnydaboy <jonathan.pi07@gmail.com>
2016-12-20 14:38:40 -0800

    Assignment 1

--- minishell.c
+++ minishell.c
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdbool.h>
 
 
 /* Constants */ 
@@ -20,8 +21,8 @@
 #define LINELEN 1024
 
 /* Prototypes */
-
-void processline (char *line);
+char ** arg_parse (char *line);
+void processline (char **line);
 
 /* Shell main */
 int main()
@@ -32,28 +33,35 @@ int main()
     while (1) {
 
         /* prompt and get line */
-	fprintf (stderr, "%% ");
-	if (fgets (buffer, LINELEN, stdin) != buffer)
-	  break;
-
+        fprintf (stderr, "%% ");
+        if (fgets (buffer, LINELEN, stdin) != buffer){
+            break;
+        }
+        
         /* Get rid of \n at end of buffer. */
-	len = strlen(buffer);
-	if (buffer[len-1] == '\n')
-	    buffer[len-1] = 0;
-
-	/* Run it ... */
-	processline (buffer);
-
+        len = strlen(buffer);
+        if (buffer[len-1] == '\n'){
+            buffer[len-1] = 0;
+        }
+        
+        char **location = arg_parse(buffer);
+        
+        /* Run it ... */
+        processline (location);
+        
+        free(location);
+        
     }
 
-    if (!feof(stdin))
+    if (!feof(stdin)) {
         perror ("read");
+    }
 
-    return 0;		/* Also known as exit (0); */
+    return 0; /* Also known as exit (0); */
 }
 
 
-void processline (char *line)
+void processline (char **line)
 {
     pid_t  cpid;
     int    status;
@@ -68,14 +76,64 @@ void processline (char *line)
     /* Check for who we are! */
     if (cpid == 0) {
       /* We are the child! */
-      execlp (line, line, (char *)0);
+      execvp ( line[0], &line[0]);
       perror ("exec");
       exit (127);
     }
     
     /* Have the parent wait for child to complete */
-    if (wait (&status) < 0)
+    if (wait (&status) < 0) {
       perror ("wait");
+    }
+}
+
+char ** arg_parse (char *line)
+{
+    int counter = 0;
+    bool inArg = false;
+    int len = strlen(line) + 1;
+    
+    for (int i = 0; i < len; i++) 
+    {
+        if (line[i] == ' ' || line[i] == 0)
+        {
+            if (inArg == true)
+            {
+                counter = counter + 1;
+                inArg = false;
+            }
+        }
+        else if (line[i] != ' ')
+        {
+            inArg = true;
+        }
+    }
+    counter = counter + 1;
+    
+    char ** ptrToStrArr = (char** ) malloc(sizeof (char*) * counter);
+    inArg = false;
+    int ptrToStrArrCounter = 0;
+    for (int i = 0; i < len; i++)
+    {
+       if (line[i] == ' ' || line[i] == 0)
+       {
+           line[i] = 0;
+           inArg = false;
+       }
+       else if (line[i] != ' ')
+       {
+           if (inArg == false)
+           {
+               ptrToStrArr[ptrToStrArrCounter] = &line[i];
+               ptrToStrArrCounter = ptrToStrArrCounter + 1;
+               inArg = true;
+           }
+       }
+    }
+    ptrToStrArr[ptrToStrArrCounter] = NULL;
+    
+    
+    return ptrToStrArr;
 }
 
 
