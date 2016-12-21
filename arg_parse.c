725fd921e5e4ee017c73a982ec788ded66915a8b
Johnnydaboy <jonathan.pi07@gmail.com>
2016-12-21 13:22:46 -0800

    Commit 4
    Recounts the number of arguments with quotes.

--- arg_parse.c
+++ arg_parse.c
@@ -11,27 +11,64 @@
 char ** arg_parse (char *line, int *argcp)
 {
     int counter = 0;
+    int quoteCount = 0;
     bool inArg = false;
+    bool inQuote = false;
     int len = strlen(line) + 1;
-    
-    for (int i = 0; i < len; i++) 
+    for (int numOfQuotes = 0; numOfQuotes < len; numOfQuotes++)
     {
-        if (line[i] == ' ' || line[i] == 0)
+        if (line[numOfQuotes] == '"')
         {
-            if (inArg == true)
+            quoteCount = quoteCount + 1;
+        }
+        
+    }
+    if ( quoteCount % 2 == 0)
+    {
+        for (int i = 0; i < len; i++) 
+        {
+            if (line[i] == '"')
             {
-                counter = counter + 1;
-                inArg = false;
+                if (inArg == false && inQuote == false)
+                {
+                    inArg = true;
+                    inQuote = true;
+                }
+                else if (inArg == true && inQuote == true)
+                {
+                    inArg = true;
+                    inQuote = false;
+                }
+                else if (inArg == true && inQuote == false)
+                {
+                    inQuote = true;
+                }
+            }
+            else if (line[i] == ' ' || line[i] == 0)
+            {
+                if (inArg == true && inQuote == false)
+                {
+                    inArg = false;
+                    counter = counter + 1;
+                }
+            }
+            else if (line[i] != ' ')
+            {
+                if (inArg == false)
+                {
+                    inArg = true;
+                }
             }
         }
-        else if (line[i] != ' ')
-        {
-            inArg = true;
-        }
     }
+    else
+    {
+        perror("You can't have an odd number of quotes \n");
+    }        
     counter = counter + 1;
-    *argcp = counter; 
-    
+    *argcp = counter;
+    printf("You have this many arguments: %d\n", counter);
+    return NULL;    
     
     char ** ptrToStrArr = (char** ) malloc(sizeof (char*) * counter);
     inArg = false;
@@ -41,7 +78,7 @@ char ** arg_parse (char *line, int *argcp)
        if (line[i] == ' ' || line[i] == 0)
        {
            line[i] = 0;
-           inArg = false;
+           inArg = false; 
        }
        else if (line[i] != ' ')
        {
